534b26567b3cc6d99dc93170480a1b6a0333681c
azizulsoftsw <azizul.softsw@gmail.com>
2020-04-30 11:13:24 +0600

    init

---
+++ themes/fclg/footer.php
@@ -0,0 +1,64 @@
+<footer class="footer-wrp">
+  <div class="ftr-top">
+    <div class="container-md">
+      <div class="row">
+        <div class="col-12">
+          <div class="ftr-col-main clearfix">
+            <div class="ftr-col ftr-col-1">
+              <h6><span>HELPFUL</span></h6>
+              <ul class="clearfix reset-list">
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">How It Works</a></li>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms and Conditions</a></li>
+              </ul>
+            </div>
+            <div class="ftr-col ftr-col-2"> 
+              <h6><span>PROFILE</span></h6>
+              <ul class="clearfix reset-list">
+                <li><a href="#">Register</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">My Account</a></li>
+              </ul>
+            </div>
+            <div class="ftr-col ftr-col-3 clearfix">
+              <h6><span>ABOUT US</span></h6>
+              <p>orem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultricies magna vitae neque porttitor eleifend. Aliquam laoreet quis urna nec molestie. Sed non odio vitae tellus interdum scelerisque id nec leo. </p>
+              <div class="ftr-socail-icon clearfix">
+                <ul class="clearfix reset-list">
+                  <li>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 </li>
+                  <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fab fa-google-plus-g"></i></a>
+                  </li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                </ul>
+              </div>              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="ftr-btm">
+    <div class="container-md">
+      <div class="row">
+        <div class="col-12">
+          <div class="ftr-btm-innr clearfix">
+            <div class="ftr-btm-col-1">
+              <p>&copy; 2020 Race-Line Competitions. All Rights Reserved.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
