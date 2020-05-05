4fc9002d49b6460073c10fa640a1c52257ed4474
Rannojit <cserannojitsarkar22@gmail.com>
2020-05-05 18:24:02 +0600

    home page 80% desktop view done

--- themes/fclg/assets/js/main.js
+++ themes/fclg/assets/js/main.js
@@ -255,6 +255,103 @@ if( $('.proGallerySlider').length ){
 }
 
 
+if( $('.hmNewCompitionsSecSlider').length ){
+    $('.hmNewCompitionsSecSlider').slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+if( $('.hmLatestWinnersSlider').length ){
+    $('.hmLatestWinnersSlider').slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+
+
+
+if( $('.mainSlider').length ){
+    $('.mainSlider').slick({
+      pauseOnHover: false,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      dots: true,
+      infinite: false,
+      arrows:false,
+      speed: 700,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade: true
+    });
+}
+
 var proTitleHeight = $('.fl-pro-grd-item-title').outerHeight();
 var proTitleHeightHalf = (proTitleHeight/2);
 $('.title-angle').css({"border-top-width": proTitleHeightHalf, "border-right-width": proTitleHeightHalf, "border-bottom-width": proTitleHeightHalf});
